c334db995b6aa5b07ae566cc89517b5e936edbdd
Silas <mangoblox.67@gmail.com>
2026-05-07 09:49:57 -0400

    commit

---
+++ LibraryApp/Program.cs
@@ -0,0 +1,36 @@
+﻿bool exited = false;
+
+while (!exited) {
+    Console.WriteLine("=== Library ===");
+}
+
+class Book {
+    public string Name { get; private set; }
+    public string Author { get; private set; }
+    public int Pages { get; private set; }
+
+    public Book(string name, string author, int pages) {
+        Name = name;
+        Author = author;
+        Pages = pages;
+    }
+}
+
+class Library {
+    private List<Book> books;
+
+    public bool AddBook(Book bookToAdd) {
+        foreach (Book book in books) {
+            if (bookToAdd.Name == book.Name) {
+                return false;
+            }
+        }
+       
+        books.Add(bookToAdd);
+        return true;
+    }
+
+
+
+    public void 
+}
